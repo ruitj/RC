1fae465736204bcebf0298482f46d66c88b7abf1
franciscogoncalves01 <61144064+franciscogoncalves01@users.noreply.github.com>
2021-12-12 18:34:55 +0000

    Update func_Client.c

--- func_Client.c
+++ func_Client.c
@@ -16,6 +16,8 @@ int validUID(char *input){
 
 int validPassword(char *input){
     int i;
+    if (strlen(input) != 9)
+        return =;
     for (i = 0; input[i] != '\n';i++){
         if (!(((isdigit(input[i])) || (isalpha(input[i]))) || (i < 8)))
             return 0;
@@ -25,43 +27,7 @@ int validPassword(char *input){
 
 int registerUser(char *input){
     char in[MAX_INPUT_SIZE], *out;
-    //int i=0;
-    //int spacesCount=0, spaceIndex=0;
-    //int status;
-    //verificacao do input (ver se tem 1 espaco, exatamente 14 carateres, UID so integers e 5 carateres e password com 8 carateres, letras e numeros)
-    /*for(i;input[i]!='\n';i++){
-        if (input[i] == ' '){
-            espacosCount++;
-            espacoIndex = i;
-        } 
-    }
-    if(i!=14 || espacoIndex!=5){
-        //print error
-        fprintf(stderr,"Error: wrong input format\n");
-        return 0;
-    }
-    if (espacosCount == 1){
-        for(i=0;i!=espacoIndex;i++){
-            if(isdigit(input[i])==0){
-                //print error
-                fprintf(stderr,"Error: wrong input format\n");
-                return 0;
-            }
-        }
-        for(i=espacoIndex+1;i<14;i++){
-            if(isalpha(input[i])==0 && isdigit(input[i])==0){
-                //print error
-                fprintf(stderr,"Error: wrong input format\n");
-                return 0;
-            }
-        }
-    }
-    else{
-        printf("Error: wrong input format\n");
-        return 0;
-        //print erro
-    }*/
-
+    
     if (!validUID(input)){
         printf("Warning: Invalid UID\n");
         return 0;
