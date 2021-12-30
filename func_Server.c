7e1487a58a90d31d7f29447226d0bdb451b1f123
lumi <73795868+lumiafonso@users.noreply.github.com>
2021-12-30 14:35:29 +0000

    Adicionei funcoes do pdf do stor

--- func_Server.c
+++ func_Server.c
@@ -42,3 +42,63 @@ char* processInput(char *input){
     }
     return out;
 }
+
+char* unregisterUserS(char *input){
+    //ir a diretoria users e apagar pasta com UID dado
+
+}
+
+int DelUserDir(char *UID){
+    char user_dirname[20];
+    sprintf(user_dirname,"USERS/%s",UID);
+    if(rmdir(user_dirname)==0)
+        return(1);
+    else
+        return(0);
+}
+
+int ListGroupsDir(GROUPLIST *list){
+    DIR *d;
+    struct dirent *dir;
+    int i=0;
+    FILE *fp;
+    char GIDname[30];
+    list->no_groups=0;
+    d = opendir("GROUPS");
+    if (d){
+        while ((dir = readdir(d)) != NULL){
+            if(dir->d_name[0]==’.’)
+                continue;
+            if(strlen(dir->d_name)>2)
+                continue;
+            strcpy(list->group_no[i], dir->d_name);
+            sprintf(GIDname,"GROUPS/%s/%s_name.txt",dir->d_name,dir->d_name);
+            fp=fopen(GIDname,"r");
+            if(fp){
+                fscanf(fp,"%24s",list->group_name[i]);
+                fclose(fp);
+            }
+            ++i;
+            if(i==99)
+                break;
+        }
+        list->no_groups=i;
+        closedir(d);
+    }
+    else
+        return(-1);
+
+    if(list->no_groups>1)
+        SortGList(list);
+
+    return(list->no_groups);
+}
+
+int DelPassFile(char *UID){
+    char pathname[50];
+    sprintf(pathname,"USERS/%s/%s_pass.txt",UID,UID);
+    if(unlink(pathname)==0)
+        return(1);
+    else
+        return(0);
+}
--- func_Server.h
+++ func_Server.h
@@ -1,1 +1,4 @@
 void processInput(char *input);
+int DelUserDir(char *UID);
+int ListGroupsDir(GROUPLIST *list);
+int DelPassFile(char *UID);
